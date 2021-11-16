14a39dea1e07e4cac5ed048fdbf9a847500fb4e8
Subhakar <75500221+karcreativeworks@users.noreply.github.com>
2021-11-16 07:59:22 +0530

    added useChain

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # dApp-reacthooks
  Snippets & React Hooks really useful for dApp developers.
+
+
+you can get chainObject from https://chainid.network/chains.json
+search for your chain and pass it to `CHAINOBJECT`
+```bash
+const { chainId, switchChain } = useChain();
+const CHAINOBJECT; 
+
+const handleSwitchChainClick = (event) => {
+    switchChain(CHAINOBJECT)
+    .then(() => {
+        console.log(chainId);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+const checkIfOnChain = () => {
+    return chainId === CHAINOBJECT.chainId;
+}
+```
---
+++ react-hooks/useChain.js
@@ -0,0 +1,63 @@
+/**
+ * useChain react-hook.
+ * use this to switch chain on metamask wallet.
+ * @module useChain
+ * @author subhakartikkireddy
+ */
+
+import { useState, useEffect } from "react";
+
+/** USE CHAIN REACT HOOK */
+export const useChain = () => {
+  const [chainId, setChainId] = useState(null);
+
+  useEffect(() => {
+    const handleChainChanged = (_chainId) => {
+      setChainId(fromHex(_chainId));
+    };
+    window.ethereum?.on("chainChanged", handleChainChanged);
+    setChainId(fromHex(window.ethereum?.chainId));
+    return () => {
+      window.ethereum?.removeListener("chainChanged", handleChainChanged);
+    };
+  }, []);
+
+  /**
+   * @param { object } chain - chain object to switch to.
+   * get your chain from here - https://chainid.network/chains.json
+   */
+  const switchChain = (chain) => {
+    const params = {
+      chainId: toHex(chain.chainId), // A 0x-prefixed hexadecimal string
+      chainName: chain.name,
+      nativeCurrency: {
+        name: chain.nativeCurrency.name,
+        symbol: chain.nativeCurrency.symbol, // 2-6 characters long
+        decimals: chain.nativeCurrency.decimals,
+      },
+      rpcUrls: chain.rpc,
+      blockExplorerUrls: [
+        chain.explorers && chain.explorers.length > 0 && chain.explorers[0].url
+          ? chain.explorers[0].url
+          : chain.infoURL,
+      ],
+    };
+    const promise = window.ethereum.request({
+      method: "wallet_addEthereumChain",
+      params: [params],
+    });
+    promise.then((chainCh) => {
+      setChainId(chain.chainId);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+    return promise;
+  };
+
+  return {
+    chainId,
+    setChainId,
+    switchChain,
+  };
+};
