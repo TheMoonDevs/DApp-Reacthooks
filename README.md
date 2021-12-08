c181863bb1e27ce19c464a4422afbd3d02e25445
Subhakar <75500221+karcreativeworks@users.noreply.github.com>
2021-12-09 02:41:37 +0530

    useContract updated.

--- README.md
+++ README.md
@@ -43,37 +43,62 @@ const checkIfOnChain = () => {
 
 ## useContract
 
+One of the most misguiding about web3.eth.contract is how it looks so simple,
+but it doesn't take many scenarios like what if the wallet disconnects, or the user is on another chain etc.
 
+use this to intialise a contract that will auto flip between web3 provider & http provider depending on whether wallet is connected or disconnected or on a different chain all together.
 
 ```bash
 import { useContract } from "react-hooks/useContract";
 
-const networkUrl, ABI_DATA, TOKEN_ADDRESS;
-const ABI_DATA;
-const TOKEN_ADDRESS; 
-const { contract } = useContract(networkUrl, ABI_DATA, TOKEN_ADDRESS);
+const rpcUrl;
+const { contract, setContractParams } = useContract(rpcUrl);
 
 ```
-you can get chainObject from https://chainid.network/chains.json
-search for your chain and pass it to `CHAINOBJECT`
+you can get rpcUrl from https://chainid.network/chains.json
+search for your chain and pass rpc field value to `rpcUrl`
+
 
 #### usage
-- change chain after user clicks a button.
+Before you can use your contract methods you need to intialise the contract 
+and for that you need to pass the following three params.
+
+- `ABI_DATA` is a js object compiled from your contract.
+- `TOKEN_ADDRESS` is the token address of your deployed contract.
+- `CHAIN_ID` is the standard chainID of the block chain you deployed on.
+you can get rpcUrl from https://chainlist.org
 ```bash
-const handleSwitchChainClick = (event) => {
-    switchChain(CHAINOBJECT)
-    .then(() => {
-        console.log(chainId);
+const ABI_DATA;
+const TOKEN_ADDRESS; 
+const CHAIN_ID; 
+
+useEffect = (() => {
+    setContractParams({
+        abi: ABI_DATA,
+        token_address: TOKEN_ADDRESS,
+        chain_id: CHAIN_ID
+    });
+},[])
+```
+- after that everything is normal
+```bash
+const mintTokenCall = () => {
+    contract
+    .methods
+    .mintToken(TOKEN_URL)
+    .send({
+        from: window.ethereum?.selectedAddress, 
+        value: VALUE
+    })
+    .once("transactionHash", (hash) => {
+        setTxProgress("Tx approved");
+    })
+    .then((tx) => {
+        setTxProgress("Tx processed");
     })
     .catch((err) => {
-        console.log(err);
+        setTxProgress("Tx unapproved or canceled");
     })
-}
-```
-- check if user is on the right chain
-```bash
-const checkIfOnChain = () => {
-    return chainId === CHAINOBJECT.chainId;
 }
 ```
 
--- react-hooks/useContract.js
+++ react-hooks/useContract.js
@@ -1,54 +1,105 @@
 /**
  * useContract react-hook.
- * use this to switch chain on metamask wallet.
- * @module useChain
+ * use this to intialise contract that will auto flip between http provider & web3 provider.
+ * @module useContract
  * @author subhakartikkireddy
  */
 
 import { useState, useEffect } from "react";
 import Web3 from "web3";
 
-export const useContract = (CHAIN_URL, ABI_DATA, TOKEN_ADDRESS) => {
-  const [web3, setWeb3] = useState(null);
-  const [contract, setContract] = useState(null);
-  const [isConnected, setIsConnected] = useState();
-  const [web3ChainUrl, setChainUrl] = useState(CHAIN_URL);
-  const [contractAbi, setContractAbi] = useState({
-    abi: ABI_DATA,
-    token_address: TOKEN_ADDRESS
+/**
+  * Summary. (useContract)
+  * Description. (initialise contract with setContractParams, pass abi, token_address, chain_id this contract lives on)
+  * @param { string } PROVIDER_URL - rpc http node url
+  * @return { Object } web3 - web3 provider, contract - contract with methods, setContractParams - initialise contract
+  * get your rpc node url from here - https://chainid.network/chains.json
+*/
+export const useContract = (PROVIDER_URL) => {
+
+  const [chainId, setChainId] = useState(null);
+  const [contractParams, setContractParams] = useState({
+    abi: null,
+    token_address: "",
+    chain_id: null
   });
 
-  useEffect(() => {
-    async function connectWeb3() {
-      try {
-        const w3 = await new Web3(window.etherium || web3ChainUrl);
-        setContract(new w3.eth.Contract(contractAbi.abi, contractAbi.token_address));
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    connectWeb3();
-  }, [contractAbi,web3ChainUrl]);
+  const [web3, setWeb3] = useState(null);
+  const [contract, setContract] = useState(null);
 
   useEffect(() => {
-    const handleDisconnect = () => {
-      setIsConnected(false);
+
+    const toHex = (num) => {
+      return "0x" + num.toString(16);
     };
-    const handleConnect = () => {
-      setIsConnected(true);
-    }
-    window?.ethereum?.on("disconnect", handleDisconnect);
-    window?.ethereum?.on("connect", handleConnect);
+
+    const fromHex = (hexString) => {
+      return parseInt(hexString, 16);
+    };
+
+    /* connect to metamask provider when on right chain */
+    const handleChainChanged = (_chainId) => {
+      setChainId(fromHex(_chainId));
+      if(fromHex(_chainId) === contractParams.chain_id){
+        setWeb3(new Web3(window.ethereum));
+      }
+      else
+        setWeb3(new Web3(PROVIDER_URL))
+    };
+
+    /* connect to metamask provider if metamask connects*/
+    const handleConnect = (connectInfo) => {
+      if(fromHex(window.ethereum.chainId) === contractParams.chain_id){
+        setWeb3(new Web3(window.ethereum));
+      }
+      else
+        setWeb3(new Web3(PROVIDER_URL))
+    };
+
+    /* connect to http provider if metamask disconnects*/
+    const handleDisconnect = (error) => {
+      setWeb3(new Web3(PROVIDER_URL));
+    };
+
+    window.ethereum?.on("connect", handleConnect);
+    window.ethereum?.on("disconnect", handleDisconnect);
+    window.ethereum?.on("chainChanged", handleChainChanged);
+
+    setChainId(fromHex(window.ethereum?.chainId));
+    if(window.ethereum?.isConnected())
+      handleConnect();
+    else 
+      handleDisconnect();
+
     return () => {
-      window?.ethereum?.removeListener("disconnect", handleDisconnect);
-      window?.ethereum?.removeListener("connect", handleConnect);
+      window.ethereum?.removeListener("connect", handleConnect);
+      window.ethereum?.removeListener("disconnect", handleDisconnect);
+      window.ethereum?.removeListener("chainChanged", handleChainChanged);
     };
-  },[window.etherium])
+  }, [setWeb3, setChainId]);
+
+  useEffect(() => {
+    if(!web3) return;
+
+    /* intitialise contract if already not initialised */
+    if(!contract && contractParams.abi && contractParams.token_address) {
+        setContract(new web3.eth.Contract(contractParams.abi, contractParams.token_address));
+    }
+    
+    /* intitialise new contract if param changes OR 
+      change provider to current if web3 changes */
+    if(contractParams.token_address !== contract?._address){
+      if(contractParams.abi && contractParams.token_address)
+        setContract(new web3.eth.Contract(contractParams.abi, contractParams.token_address));
+    }
+    else {
+      contract.setProvider(web3.givenProvider);
+    }
+  }, [web3, contract, contractParams]);
 
   return {
     web3,
     contract,
-    setContractAbi,
-    setChainUrl
+    setContractParams
   };
 };
